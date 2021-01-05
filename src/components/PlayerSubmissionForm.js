5d9a43af5db590c36e29941e83b1684a14403aeb
Lina Do <l.do5147@gmail.com>
2021-01-05 12:56:22 -0800

    set up a state for the players form and filled out the name and value for each input

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -4,6 +4,18 @@ import PropTypes from 'prop-types';
 import './PlayerSubmissionForm.css';
 
 const PlayerSubmissionForm = () => {
+
+  const [playerForm, setPlayerForm] = useState( {
+    adj1: '',
+    noun1: '',
+    adv: '',
+    verb: '',
+    adj2: '',
+    noun2: ''
+  })
+
+
+
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
@@ -18,22 +30,34 @@ const PlayerSubmissionForm = () => {
           The
           <input
             placeholder="adjective"
+            name="adj1"
+            value={playerForm.adj1}
             type="text" />
           <input
             placeholder="noun"
+            name='noun1'
+            value={playerForm.noun1}
             type="text" />
           <input
             placeholder="adverb"
+            name='adv'
+            value={playerForm.adv}
             type="text" />
           <input
             placeholder="verb"
+            name='verb'
+            value={playerForm.verb}
             type="text" />
           the
           <input
             placeholder="adjective"
+            name='adj2'
+            value={playerForm.adj2}
             type="text" />
           <input
             placeholder="noun"
+            name='noun2'
+            value={playerForm.noun2}
             type="text" />
           .
         </div>
